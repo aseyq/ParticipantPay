5b918ee7061a3e4104f16e7b3fee6d3eefd4c525
Ali Seyhun Saral <aseyq@posteo.net>
2024-04-03 23:52:25 +0200

    missing rounding

--- R/create_prolific_files.R
+++ R/create_prolific_files.R
@@ -8,6 +8,11 @@
 #' @return NULL
 #' @export
 create_prolific_files <- function(df, ProlificIDColumn, bonusColumn, output_folder=".") {
+
+  # round bonus column
+  df <- df %>%
+    dplyr::mutate(!!rlang::sym(as.character(ensym(bonusColumn))) := round(!!rlang::sym(as.character(ensym(bonusColumn))), 2))
+    
   dplyr::select(df, {{ProlificIDColumn}}) %>%
     readr::write_csv(paste0(output_folder, "/bulk_approval_", format(Sys.time(), "%Y%m%d_%H%M%S"), ".txt"), col_names = FALSE)
 
